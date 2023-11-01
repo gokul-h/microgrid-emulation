d103fac78309d78a79ef02408049101156002b39
Gokul H <72368205+gokul-h@users.noreply.github.com>
2023-11-01 19:55:58 +0530

    new

--- battery_new_event.py
+++ battery_new_event.py
@@ -9,17 +9,20 @@ import csv
 from azure.eventhub import EventHubProducerClient, EventData
 
 # Create a producer client to produce and publish events to the event hub.
-producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://analysisevent.servicebus.windows.net/;SharedAccessKeyName=sample;SharedAccessKey=on7aFNnRgq+myO74H2LqY7K7mHeelnODG+AEhNsLvmg=;EntityPath=battery", eventhub_name="battery")
+producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://microgrid.servicebus.windows.net/;SharedAccessKeyName=grid;SharedAccessKey=4w9ZjvDa8OPNhANa3Mpkx4nzCmt90XbQi+AEhJCcxt8=;EntityPath=grid", eventhub_name="grid")
 
 # Open the CSV file and read each line.
-with open('data.csv', 'r') as file:
+with open('NOV_2022.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row.
     for i, row in enumerate(reader):
         # Convert each row to a dictionary.
-        reading = {'Battery_Active_Power': row[0], 'Battery_Active_Power_Set_Response': row[1]}
+        reading = {'Timestamp': row[0],
+                   'Battery_Active_Power': row[1], 
+                   'Battery_Active_Power_Set_Response': row[2]}
         s = json.dumps(reading)  # Convert the reading into a JSON string.
-        print("Sending data " , i)
+        # debugging line
+        print("Sending data " , i," ", row[0])
         # Create a batch and add event data to it.
         event_data_batch = producer.create_batch()
         event_data_batch.add(EventData(s))
