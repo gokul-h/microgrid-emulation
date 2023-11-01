0a99eb6eff2efee8e1214b2380c4e45930076699
Gokul H <72368205+gokul-h@users.noreply.github.com>
2023-11-01 14:12:03 +0530

    delay

--- battery_new_event.py
+++ battery_new_event.py
@@ -11,14 +11,11 @@ from azure.eventhub import EventHubProducerClient, EventData
 # Create a producer client to produce and publish events to the event hub.
 producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://analysisevent.servicebus.windows.net/;SharedAccessKeyName=sample;SharedAccessKey=on7aFNnRgq+myO74H2LqY7K7mHeelnODG+AEhNsLvmg=;EntityPath=battery", eventhub_name="battery")
 
-# Open the CSV file and read the first 10 lines.
+# Open the CSV file and read each line.
 with open('data.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row.
     for i, row in enumerate(reader):
-        if i == 10:  # Stop after reading 10 data rows.
-            break
-
         # Convert each row to a dictionary.
         reading = {'Battery_Active_Power': row[0], 'Battery_Active_Power_Set_Response': row[1]}
         s = json.dumps(reading)  # Convert the reading into a JSON string.
@@ -29,6 +26,9 @@ with open('data.csv', 'r') as file:
 
         # Send the batch of events to the event hub.
         producer.send_batch(event_data_batch)
+
+        # Wait for 5 seconds before sending the next row.
+        time.sleep(5)
 
 # Close the producer.    
 producer.close()
